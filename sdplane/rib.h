2d314256be06d2aab8636901a4eade3bb550aeb0
Yasuhiro Ohara <yasu1976@gmail.com>
2025-05-07 00:43:51 +0900

    vswitch_link design for vlan support.

--- sdplane/rib.h
+++ sdplane/rib.h
@@ -1,23 +1,17 @@
 #ifndef __RIB_H__
 #define __RIB_H__
 
-#define MAX_TAPIF_PORTS 16
+#define MAX_TAP_IF 8
+#define MAX_ROUTER_IF 8
 #define MAX_VSWITCH_PORTS 4
 #define MAX_VSWITCH_ID 4
-
+#define MAX_VSWITCH_LINK 32
 #define MAX_VLAN_PER_PORT 4
 #define MAX_ETH_PORTS 8
 
-struct tapif_conf
+struct tap_if_conf
 {
   int sockfd; //tap sockfd.
-};
-
-struct switch_port
-{
-  uint8_t type; // dpdk or tapif
-  uint8_t portid;
-  uint8_t queueid;
 };
 
 struct router_if
@@ -28,17 +22,19 @@ struct router_if
   struct in6_addr ipv6_addr;
 };
 
-struct vswitch_conf
-{
-  uint16_t port_size;
-  struct switch_port port[MAX_VSWITCH_PORTS];
-};
-
 struct vswitch_link
 {
+  uint16_t port_id;
   uint16_t vlan_id;
+  uint16_t tag_id; //0 indicates untag. tag_id != vlan_id is tag-modify.
   uint16_t vswitch_id;
   uint16_t vswitch_port;
+};
+
+struct vswitch_conf
+{
+  uint16_t vswitch_port_size;
+  struct vswitch_link vswitch_port[MAX_VSWITCH_PORTS];
 };
 
 struct port_conf
@@ -48,14 +44,10 @@ struct port_conf
   struct rte_eth_link link;
   struct rte_eth_dev_info dev_info;
 
-  /* link to upper device. */
-  uint16_t type; //l2_vswitch_port or l3_router_if (direct).
-  uint16_t router_if;
-  struct vswitch_link switch_link; //for untag.
-
-  /* tx/rx vlan config */
+  /* vlan support. */
   uint16_t vlan_size; //configured vlan size.
   struct vswitch_link vlan_link[MAX_VLAN_PER_PORT];
+  struct vswitch_link switch_link; //for untag.
 };
 
 #include "queue_config.h"
@@ -70,10 +62,12 @@ struct rib_info {
   uint32_t ver;
   uint8_t tapif_size;
   uint8_t vswitch_size;
+  uint8_t vswitch_link_size;
   uint8_t port_size;
   uint8_t lcore_size;
-  struct tapif_conf tapif[MAX_TAPIF_PORTS];
+  struct tap_if_conf tap_if[MAX_TAP_IF];
   struct vswitch_conf vswitch[MAX_VSWITCH_ID];
+  struct vswitch_link vswitch_link[MAX_VSWITCH_LINK];
   struct port_conf port[MAX_ETH_PORTS];
   struct lcore_qconf lcore_qconf[RTE_MAX_LCORE];
 } __rte_cache_aligned;
