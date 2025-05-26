109eeacf96a8b754dcb5ba73c3747f6e93c43789
k1yoto <nakatakiyoto2022@gmail.com>
2025-05-26 07:25:12 +0000

    chore(vty_shell): remove duplicate set-worker

--- sdplane/vty_shell.c
+++ sdplane/vty_shell.c
@@ -187,7 +187,6 @@ vty_shell (void *arg)
   INSTALL_COMMAND2 (shell->cmdset, shutdown_cmd);
 
   INSTALL_COMMAND2 (shell->cmdset, show_worker);
-  INSTALL_COMMAND2 (shell->cmdset, set_worker);
   INSTALL_COMMAND2 (shell->cmdset, start_stop_worker);
 
   INSTALL_COMMAND2 (shell->cmdset, debug_zcmdsh);
