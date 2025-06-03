3c8288faae0684b3b87f71735bc72d375dc4e8dd
saza-ku <the.latticeheart@gmail.com>
2025-06-03 18:26:18 +0900

    build tests

--- tests/test_l2repeater.c
+++ tests/test_l2repeater.c
@@ -1,5 +1,19 @@
+#include "sdplane_version_for_test.h"
+
+#include <rte_common.h>
+#include <rte_ether.h>
+#include <rte_ethdev.h>
+#include <rte_mbuf.h>
+#include <rte_mempool.h>
+#include <rte_eal.h>
+
 #include <stdio.h>
+#include "l2_repeater.h"
 
 int main() {
     printf("Hello, World!\n");
+
+    rte_eal_init(0, NULL);
+
+    l2_repeater(NULL);
 }
