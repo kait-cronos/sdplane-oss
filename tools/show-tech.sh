24966813a7b4bad672da1334915e747cdfa71131
Yasuhiro Ohara <yasu1976@gmail.com>
2025-10-11 17:47:00 +0900

    update show-tech.sh.

--- tools/show-tech.sh
+++ tools/show-tech.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+#set -e
 
 cmd=""
 
@@ -10,8 +10,30 @@ print_cmd_and_exec() {
   echo ""
 }
 
+dir=/var/tmp
+host=`hostname`
+machine=`sh config.guess`
+date=`date "+%F-%T"`
+filename=`echo show-tech-${host}-${date}.txt`
+
+echo "saving in ..."
+echo $dir/$filename
+
+{
+
+echo $filename
+echo ""
+
+echo "*******"
+cmd="hostname --fqdn"
+print_cmd_and_exec
+
 echo "*******"
 cmd="lsb_release -a"
+print_cmd_and_exec
+
+echo "*******"
+cmd="uname -a"
 print_cmd_and_exec
 
 echo "*******"
@@ -23,7 +45,7 @@ cmd="free -h"
 print_cmd_and_exec
 
 echo "*******"
-cmd="dmidecode -t system"
+cmd="sudo dmidecode -t system"
 print_cmd_and_exec
 
 echo "*******"
@@ -35,11 +57,15 @@ cmd="df -h"
 print_cmd_and_exec
 
 echo "*******"
-cmd="dpkg -l"
+cmd="dpkg -l --no-pager"
 print_cmd_and_exec
 
 echo "*******"
 cmd="pkg-config --modversion libdpdk"
+print_cmd_and_exec
+
+echo "*******"
+cmd="dpdk-devbind.py --status-dev net"
 print_cmd_and_exec
 
 echo "*******"
@@ -57,4 +83,47 @@ print_cmd_and_exec
 echo "*******"
 cmd="ip route show"
 print_cmd_and_exec
+
+echo "*******"
+for file in `ls /etc/netplan/*.yaml`; do
+  cmd="sudo cat $file"
+  print_cmd_and_exec
+done
+
+echo "*******"
+if [ -f /var/run/sdplane.pid ]; then
+  cmd="cat /var/run/sdplane.pid"
+  print_cmd_and_exec
+  pid=`cat /var/run/sdplane.pid`
+  cmd="ps -lp $pid"
+  print_cmd_and_exec
+else
+  echo "no sdplane pid file."
+fi
+
+echo "*******"
+dirs="/lib /usr/sbin /usr/lib /usr/local/lib /usr/local/sbin"
+cmd="find $dirs -type f -name \"*sdplane*\" | xargs ls -lh"
+print_cmd_and_exec
+
+echo "*******"
+execs=`find /usr -type f -name sdplane`
+for exec in $execs; do
+  cmd="ldd $exec"
+  print_cmd_and_exec
+done
+#for exec in $execs; do
+#  cmd="$exec -v"
+#  print_cmd_and_exec
+#done
+
+echo "*******"
+cmd="systemctl status sdplane"
+print_cmd_and_exec
+
+echo "*******"
+echo "show tech completed."
+
+# } 2>&1 | tee $dir/$filename
+} > $dir/$filename 2>&1
 
